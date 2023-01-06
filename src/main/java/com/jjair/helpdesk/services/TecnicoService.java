8075582b7e23e398b4f9ff3941fb9c93f8ac8863
jjairlima <softcon@gmail.com>
2023-01-06 01:27:46 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/jjair/helpdesk/services/TecnicoService.java
+++ src/main/java/com/jjair/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jjair.helpdesk.domain.Tecnico;
 import com.jjair.helpdesk.repositories.TecnicoRepository;
+import com.jjair.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado! Id:" + id));
 	}
 }
