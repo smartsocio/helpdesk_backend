67815e5a8c5c924d70532d1f0b5ac7e1dd61d48a
jjairlima <softcon@gmail.com>
2023-01-09 23:36:15 -0300

    FindById para Chamado

---
+++ src/main/java/com/jjair/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.jjair.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jjair.helpdesk.domain.Chamado;
+import com.jjair.helpdesk.repositories.ChamadoRepository;
+import com.jjair.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+	
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrato! ID:" + id));
+	}
+}
